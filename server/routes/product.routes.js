3e78fff90c9a4c7447b35369090cdf1a2e966100
MmedaraJ <72234561+MmedaraJ@users.noreply.github.com>
2023-01-06 21:39:47 -0500

    Update product.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -5,8 +5,8 @@ module.exports = function(app){
     app.get('/api', ProductController.index);
     //use post req to create
     //authenticate every route except login and register, etc
-    app.get('/api/products/:id', authenticate, ProductController.getProduct);
-    app.post('/api/products/new', authenticate, ProductController.createProduct);
+    app.get('/api/products/:id', /* authenticate, */ ProductController.getProduct);
+    app.post('/api/products/new', /* authenticate, */ ProductController.createProduct);
     app.get('/api/products', authenticate, ProductController.getAllProducts);
-    app.put('/api/products/update/:id', authenticate, ProductController.updateProduct);
+    app.put('/api/products/update/:id', /* authenticate, */ ProductController.updateProduct);
 }
